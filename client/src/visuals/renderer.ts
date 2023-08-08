9f09eb7e9e2221cfd56a72635c5c72b4a9d92c1f
Konrad Hapke <hap19520@spengergasse.at>
2023-08-08 17:49:55 +0200

    join ui progress

---
+++ client/src/visuals/renderer.ts
@@ -0,0 +1,24 @@
+class RendererError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.name = "RendererError";
+    }
+}
+
+class Image {
+    constructor() {
+
+    }
+}
+
+export class Renderer {
+    static async init(canvas: HTMLCanvasElement) {
+        const gpu = navigator.gpu;
+        const ctx = canvas.getContext('webgpu');
+        if (!ctx || !gpu)
+            throw new RendererError('WebGPU is not supported');
+
+        const adapter = await gpu.requestAdapter();
+
+    }
+}
